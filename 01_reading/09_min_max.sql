6a95ae6ee3ce85be43e24e818acda58ce1a6bf23
angel melendez <angelmelen88@gmail.com>
2024-05-05 13:09:01 -0600

    feat:file min and max

---
+++ 01_reading/09_min_max.sql
@@ -0,0 +1,7 @@
+-- MIN MAX 
+
+-- Obtiene el valor menor del campo edad de la tabla "users"
+SELECT MIN(age) FROM users;
+
+-- obtiene el valor mayor del campo edad de la tabla "users"
+SELECT MAX(age) FROM users;
