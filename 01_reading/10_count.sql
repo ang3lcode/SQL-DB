bdaa81665b468867d5de1c27a642e15779b26c12
angel melendez <angelmelen88@gmail.com>
2024-05-06 20:53:52 -0600

    feat:file count

---
+++ 01_reading/10_count.sql
@@ -0,0 +1,7 @@
+-- COUNT
+
+-- Cuenta cuantas filas contiene la tabla "users"
+SELECT COUNT(*) FROM users;
+
+-- cuenta cuantas filas contiene un dato no nulo  en el campo edad de la tabla "users"
+SELECT COUNT(age) FROM users;
